d4ff163b33d4ac7a74962bdaa533e64c4fa50f83
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2022-12-16 22:43:35 +0700

    🛠 Fixing security risk in settings ------------------------------------------------------------------------ 🛠 Fixing expose sensitive data in setting by storing them in and as .env variable

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,11 +167,11 @@ SITE_ID = 1
 
 OLD_PASSWORD_FIELD_ENABLED = True
 
-DEFAULT_FROM_EMAIL = 'si.mbe.mulya.motor@gmail.com'
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'si.mbe.mulya.motor@gmail.com'
-EMAIL_HOST_PASSWORD = 'qyuonwojkfwqvask'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
