ed305047e6cd5bc66895dc6d0c6a17968c6b1549
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2023-01-12 13:52:13 +0700

    ⚙ Configure Models To Django Admin Panel

--- backend/si_mbe/admin.py
+++ backend/si_mbe/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from si_mbe.models import (Brand, Logs, Profile, Restock, Restock_detail,
-                           Sales, Sales_detail, Sparepart, Supplier)
+from si_mbe.models import (Brand, Category, Customer, Logs, Mechanic, Profile,
+                           Restock, Restock_detail, Sales, Sales_detail,
+                           Salesman, Service, Service_action,
+                           Service_sparepart, Sparepart, Supplier)
 
 
 # Register your models here.
@@ -40,6 +42,34 @@ class SupplierAdmin(admin.ModelAdmin):
     readonly_fields = ['supplier_id']
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ['category_id']
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    readonly_fields = ['customer_id']
+
+
+class MechanicAdmin(admin.ModelAdmin):
+    readonly_fields = ['mechanic_id']
+
+
+class SalesmanAdmin(admin.ModelAdmin):
+    readonly_fields = ['salesman_id']
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ['service_id']
+
+
+class ServiceActionAdmin(admin.ModelAdmin):
+    readonly_fields = ['service_action_id']
+
+
+class ServiceSparepartAdmin(admin.ModelAdmin):
+    readonly_fields = ['service_sparepart_id']
+
+
 admin.site.register(Brand, BrandAdmin)
 admin.site.register(Logs, LogsAdmin)
 admin.site.register(Profile, ProfileAdmin)
@@ -49,3 +79,10 @@ admin.site.register(Sales, SalesAdmin)
 admin.site.register(Sales_detail, SalesDetailAdmin)
 admin.site.register(Supplier, SupplierAdmin)
 admin.site.register(Sparepart, SparepartAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Mechanic, MechanicAdmin)
+admin.site.register(Salesman, SalesmanAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Service_action, ServiceActionAdmin)
+admin.site.register(Service_sparepart, ServiceSparepartAdmin)
